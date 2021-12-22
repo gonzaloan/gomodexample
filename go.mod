157d1f44bb465459f261632ebc4bf760ae83c947
Gonzalo Muñoz <gonzalo.munoz0@walmart.com>
2021-12-22 10:24:03 -0300

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gonzaloan/gomodexample
+
+go 1.17
---
+++ utils.go
@@ -0,0 +1,8 @@
+package utils
+
+import "fmt"
+
+func HelloWorld() {
+	fmt.Println("Hello World from Test Mod")
+}
+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
